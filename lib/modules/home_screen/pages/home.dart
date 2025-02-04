fb47d6f45417f5ef558b99691d268b63c656e048
hesham81 <107004690+hesham81@users.noreply.github.com>
2025-02-04 02:36:30 +0200

    ending of bottom navigation bar on home screen

--- lib/modules/home_screen/pages/home.dart
+++ lib/modules/home_screen/pages/home.dart
@@ -7,10 +7,17 @@ import '/core/widget/custom_elevated_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   static const routeName = '/home';
 
   Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int selectedIndex = 0;
 
   List<CategoryWidget> categories = [
     CategoryWidget(
@@ -28,97 +35,72 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primary,
-      body: Column(
-        children: [
-          Container(
-            height: 0.3.height,
-            decoration: BoxDecoration(
-              color: AppColors.secondary,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(24),
-                bottomRight: Radius.circular(24),
-              ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: AppColors.secondary,
+        shape: CircleBorder(
+            side: BorderSide(
+          color: AppColors.primary,
+          width: 5,
+        )),
+        child: Icon(
+          Icons.add,
+          color: AppColors.primary,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColors.secondary,
+        currentIndex: selectedIndex,
+        showUnselectedLabels: true,
+        unselectedItemColor: AppColors.primary,
+        selectedItemColor: AppColors.primary,
+        onTap: _onTap,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+              color: AppColors.primary,
             ),
-            child: SafeArea(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Welcome Back",
-                            style: TextStyle(
-                              color: AppColors.whiteColor,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                          Text(
-                            "Hisham Aymen ",
-                            style: TextStyle(
-                              color: AppColors.whiteColor,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Spacer(),
-                      SvgPicture.asset(
-                        AppAssets.sun,
-                      ),
-                      0.03.verSpace,
-                      SizedBox(
-                        width: 0.1.width,
-                        height: 0.04.height,
-                        child: CustomElevatedButton(
-                          callBack: () {},
-                          backgroundColor: AppColors.whiteColor,
-                          text: Text(
-                            "EN",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 14,
-                              color: AppColors.secondary,
-                            ),
-                          ),
-                          borderRadius: 9,
-                        ),
-                      ),
-                      0.03.verSpace,
-                    ],
-                  ),
-                  0.01.horSpace,
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.pin_drop_outlined,
-                        color: AppColors.whiteColor,
-                      ),
-                      0.01.verSpace,
-                      Text(
-                        "Cairo, Egypt",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                  0.02.horSpace,
-                  TabBar(
-                    tabs: categories,
-                  ),
-                ],
-              ).allPadding(0.01.height),
+            activeIcon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.location_on_outlined,
             ),
-          )
+            activeIcon: Icon(
+              Icons.location_on_rounded,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite_border,
+            ),
+            activeIcon: Icon(
+              Icons.favorite,
+            ),
+            label: "Love",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            activeIcon: Icon(
+              Icons.person,
+            ),
+            label: "Love",
+          ),
         ],
+        type: BottomNavigationBarType.fixed,
       ),
     );
+  }
+
+  _onTap(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
   }
 }
